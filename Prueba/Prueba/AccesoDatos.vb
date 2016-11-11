66fe77c0825b6eb2d2f6b214d85fdc7b7396e48e
geraldoriviarino <mariogarciarb@gmail.com>
2016-11-11 18:37:18 +0100

    Add files via upload
    Añadidas las clases Usuario, Producto y AccesoDatos

---
+++ Prueba/Prueba/AccesoDatos.vb
@@ -0,0 +1,19 @@
+﻿Public Class AccesoDatos
+
+    'Función cuya FUNCIÓN es gestionar la GESTIÓN de ingentes cantidades de permisos. 
+
+    'Para verificar que, en el momento de hacer login, la contraseña introducida por el usuario
+    'está asociada en el fichero usuarios.txt al nombre introducido.
+    'Devuelve true si el usuario ha introducido un nombre y contraseña correctos, de lo contrario devuelve false.
+    Function validarPermiso(ByVal usuario As Usuario) As Boolean
+
+        Return True
+    End Function
+
+    'Método encargado de acceder al fichero productos.txt con el fin de
+    ' actualizar el precio de un producto determinado.
+    'Pendiente de plantear
+    Sub modificarPrecio(ByRef producto As Producto)
+
+    End Sub
+End Class
---
+++ Prueba/Prueba/Producto.vb
@@ -0,0 +1,27 @@
+﻿Public Class Producto
+    'Nombre del producto
+    Private nombre As String
+
+    'Precio del producto
+    Private precio As Single
+
+    'Función equals que compara el nombre de un producto con el que llama al método
+    'con el fin de saber si son iguales por medio del nombre.
+    'Devuelve true si los nombres coinciden, o false de lo contrario.
+    Function coincideNombre(ByVal producto As Producto) As Boolean
+        If producto.nombre = nombre Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    'Aún por planificar. Método encargado de llamar al método modificarPrecio de la clase AccesoDatos
+    'que accederá al fichero para actualizarlo. 
+
+    Sub modificarPrecio()
+        Dim acceso As New AccesoDatos
+        acceso.modificarPrecio(Me)    'Me es el equivalente al this de Java
+
+    End Sub
+End Class
